2df44f0a34bb5f578a3dbb052c5644835abcfe1c
Kelly Selden <kellyselden@gmail.com>
2020-09-20 16:14:47 +0100

    chore: add test

---
+++ test/index-test.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const { describe } = require('./helpers/mocha');
+const { expect } = require('./helpers/chai');
+
+describe(function() {
+  it('works', function() {
+    expect(1).to.equal(1);
+  });
+});
