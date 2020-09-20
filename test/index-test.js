159d202bb7e6e734df4af6743715a7b1bb5e26c8
Kelly Selden <kellyselden@gmail.com>
2020-09-20 16:24:31 +0100

    set up git test

--- test/index-test.js
+++ test/index-test.js
@@ -3,9 +3,21 @@
 const { describe } = require('./helpers/mocha');
 const { expect } = require('./helpers/chai');
 const index = require('..');
+const { createTmpDir } = require('./helpers/tmp');
+const { gitInit } = require('git-fixtures');
+const execa = require('execa');
 
 describe(function() {
-  it('works', function() {
+  it('works', async function() {
+    let tmpPath = await createTmpDir();
+
+    await gitInit({ cwd: tmpPath });
+
     expect(index).to.equal(1);
+    expect(
+      (await execa('git', ['status'], {
+        cwd: tmpPath
+      })).stdout
+    ).to.include('No commits yet');
   });
 });
