602830dfb81e4101bfd7bfa54080a3ae099bf069
Kelly Selden <kellyselden@gmail.com>
2020-10-03 18:52:33 +0100

    chore: use `getTags` in tests

--- src/index.js
+++ src/index.js
@@ -1,43 +1,11 @@
 'use strict';
 
 const execa = require('execa');
-const { EOL } = require('os');
 const semver = require('semver');
-
-async function getTags(tmpPath) {
-  let { stdout } = await execa('git', [
-    'for-each-ref',
-    '--sort',
-    '-v:refname',
-    '--format',
-    '%(*objectname) %(tag) %(subject)',
-    'refs/tags'
-  ], {
-    cwd: tmpPath
-  });
-
-  let lines = stdout.split(EOL);
-
-  let tags = lines.map(line => {
-    let [commit, tag, ...message] = line.split(' ');
-
-    return {
-      commit,
-      tag,
-      message: message.join(' ')
-    };
-  });
-
-  return tags;
-}
-
-async function getTagMessage(tag, cwd) {
-  let message = (await execa('git', ['for-each-ref', `refs/tags/${tag}`, '--format=%(contents)'], {
-    cwd
-  })).stdout.trim();
-
-  return message;
-}
+const {
+  getTags,
+  getTagMessage
+} = require('./git');
 
 async function index({
   cwd: tmpPath = process.cwd(),
--- test/helpers/chai.js
+++ test/helpers/chai.js
@@ -1,5 +1,26 @@
 'use strict';
 
 const chai = require('chai');
+const sinon = require('sinon');
+
+// This should be replaced by Chai v5
+// https://github.com/chaijs/chai/issues/644
+chai.use((_chai, utils) => {
+  chai.Assertion.addMethod('match', function match(expectedMatch) {
+    let subject = utils.flag(this, 'object');
+    let spy = sinon.spy();
+    spy(subject);
+    try {
+      sinon.assert.calledWithMatch(spy, expectedMatch);
+    } catch (error) {
+      error.name = 'MatchAssertionError';
+      error.message = error.message.replace(
+        /^expected spy to be called with match/,
+        `expected ${utils.objDisplay(subject)} to match`
+      );
+      throw error;
+    }
+  });
+});
 
 module.exports = chai;
